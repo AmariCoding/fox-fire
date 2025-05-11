9e87746e91c35653ad378a47b20cbda7803b4a99
Jerome <jerome.guya@evsu.edu.ph>
2025-05-11 14:01:07 +0800

    initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+  return (
+    <nav className="bg-orange-500 shadow-lg">
+      <div className="max-w-6xl mx-auto px-4">
+        <div className="flex justify-between">
+          <div className="flex space-x-7">
+            <div className="flex items-center py-4 px-2">
+              <img src="/logo.png" alt="logo" className="h-10" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
